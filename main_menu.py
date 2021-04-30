e8d9324997f0528ef61d225b5cc09035c9658c9d
Van Truong <vantruong@Vans-MacBook-Pro.local>
2021-04-30 13:32:02 -0700

    Finished

--- main_menu.py
+++ main_menu.py
@@ -1,14 +1,15 @@
+import time
 import problem
 
 def main_menu():
-    # Need to chagne it a little to ensure user dont put wild inputs
-    print('Welcome to 861279550 8 puzzle solver.')
+    # Check whether we need to make synamic size
+    print('Welcome to 861279550\'s 8 puzzle solver.')
     puzzle_type = input('Type "1" to use a default puzzle, or "2" to enter your own puzzle:\n')
 
     matrix = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,0]
+        [1,0,3],
+        [4,2,6],
+        [7,5,8]
     ]
 
     if puzzle_type == '2':
@@ -24,44 +25,44 @@ def main_menu():
         return
 
     print('Enter your choice of algorithm')
-    algo_choice = input('Uniform Cost Search\nA* with the Misplaced Tile heuristic\nA* with the Euclidian distance heuristic\n')
+    algo_choice = input('Uniform Cost Search\nA* with the Misplaced Tile heuristic\nA* with the Euclidean Distance heuristic\n')
 
     puzzle = problem.Problem(matrix)
-    #based on algo_choice, call respective search function
+    print('\n')
+
+    start_time = time.time()
+    
+    # Based on algo_choice, call respective search function
     if algo_choice == '1':
-        node = problem.ucs(puzzle)
+        print('Starting Uniform Cost Search with:')
+        problem.print_state(puzzle.puzzle)
+        print('\n')
+        (node, expanded_children, max_q_size) = problem.ucs(puzzle)
     elif algo_choice == '2':
-        node = problem.misplaced(puzzle)
+        print('Starting A* with the Misplaced Tile heuristic with:')
+        problem.print_state(puzzle.puzzle)
+        print('\n')
+        (node, expanded_children, max_q_size) = problem.misplaced(puzzle)
     elif algo_choice == '3':
-        node = problem.heuristic(puzzle)
+        print('Starting A* with the Euclidean Distance heuristic with:')
+        problem.print_state(puzzle.puzzle)
+        print('\n')
+        (node, expanded_children, max_q_size) = problem.euclidean(puzzle)
     else:
         print("You crashed the game!")
         return
 
-    # test = problem.Problem(matrix)
-    # problem.print_state(test.puzzle)
-    # print('\n')
-    # # print(test.blank_x, test.blank_y)
-    # test.go_up()
-    # problem.print_state(test.puzzle)
-    # print('\n')
-    # test.go_down()
-    # problem.print_state(test.puzzle)
-    # print('\n')
-    # test.go_left()
-    # problem.print_state(test.puzzle)
-    # print('\n')
-    # test.go_right()
-    # problem.print_state(test.puzzle)
-    # print('\n')
-    # test.go_right()
-    # problem.print_state(test.puzzle)
-    # print(x)
-    # x.go_up()
-    # print(x)
-    # print(problem.find_zero(matrix))
-    # problem.print_state(x.init_state.puzzle)
-    # print('Problem\'s initial state:', x.init_state.puzzle)
-    # print('Problem\'s goal state:', x.goal_state.puzzle)
+    if not bool(node):
+        print('No solution was found.')
+        return
+
+    print('\nGoal!! Goal state:')
+    problem.print_state(node.problem.puzzle)
+    print('\nTo solve this problem the search algorithm expanded a total of', expanded_children, 'nodes')
+    print('The maximum number of nodes in the queue at any one time:', max_q_size)
+
+
+    print(time.time() - start_time, 'seconds')
+
 
 main_menu()
--- problem.py
+++ problem.py
@@ -1,18 +1,14 @@
-# This generates the whole solution
-    # Starting with init_state
-    # For every operator generate a new state - This is using the algorithm?
-    # If the state we arrive at matches the goal state then we found the solution
-
-# import node
 import sys
+import math
+from copy import deepcopy
 
 _max = sys.maxsize
 
 expanded_children = 0
-children_to_expand = [] #frontier
+max_q_size = 0
 
+# ---------------------- Problem Class ----------------------  #
 class Problem:
-    #curr_state = [] # changed from init_state
     goal_state = [
         [1,2,3],
         [4,5,6],
@@ -30,6 +26,8 @@ class Problem:
             self.swap_tile(self.blank_x - 1, self.blank_y) # col = x axis, row = y axis
             self.blank_x -= 1
             return True
+        else:
+            return False
 
     # 0 in last row -> cannot go down
     def go_down(self):
@@ -38,6 +36,8 @@ class Problem:
             self.swap_tile(self.blank_x + 1, self.blank_y)
             self.blank_x += 1
             return True
+        else:
+            return False
 
     # 0 in 1st col -> cannot go left
     def go_left(self):
@@ -69,23 +69,60 @@ class Problem:
         temp = self.puzzle[self.blank_x][self.blank_y]
         self.puzzle[self.blank_x][self.blank_y] = self.puzzle[new_x][new_y]
         self.puzzle[new_x][new_y] = temp
+# ------------------------------------------------------------------------------ #
 
+
+# ---------------------- Node Class ----------------------  #
 class Node:
-    # Construct a state, which is a node
-    # def __init__(self, row1, row2, row3):
-    #     self.puzzle_state = [row1, row2, row3]
 
     def __init__(self, problem, path_cost, heuristic_cost):
-        self.puzzle = problem
-        self.parent = []
+        self.problem = problem
         self.path_cost = path_cost
         self.heuristic_cost = heuristic_cost
 
-    # expand node
+    def create_child(node):
+        # Need a deep copy because we have a list of list
+        # Also, we don't want it to reference the og copy
+        new_child = deepcopy(node)
+        new_child.path_cost += 1
+        return new_child
+
+    def expand(node):
+        global expanded_children
+        new_children = []
+
+        # Check whether an operation was successfully applied
+        if node.problem.go_up():
+            new_children.append(Node.create_child(node))
+            node.problem.go_down() # Undo the move so the next operation can be applied
+        if node.problem.go_down():
+            new_children.append(Node.create_child(node))
+            node.problem.go_up()
+        if node.problem.go_left():
+            new_children.append(Node.create_child(node))
+            node.problem.go_right()
+        if node.problem.go_right():
+            new_children.append(Node.create_child(node))
+            node.problem.go_left()
+        
+        # expanded_children += len(new_children)
+        expanded_children += 1
+        return new_children
+
+    # Need to choose a leaf node that has the least cost to remove and expand next
+    def deque(nodes):
+        lowest_cost = _max
+        node_to_remove = nodes[0]
+        for node in nodes:
+            if (node.path_cost + node.heuristic_cost) < lowest_cost:
+                lowest_cost = node.path_cost + node.heuristic_cost
+                node_to_remove = node
+        nodes.remove(node_to_remove)
+        return node_to_remove
+# ------------------------------------------------------------------------------ #
 
 
-
-# Helpers
+# ---------------------- Helper Functions ----------------------  #
 def print_state(matrix):
     for row in matrix:
         print(*row, sep=' ')
@@ -96,24 +133,149 @@ def find_zero(matrix):
             if matrix[x][y] == 0:
                 return  x, y
 
-def pop_node(nodes):
-    lowest_cost = _max
-    index_node_to_pop = 0
-    for index in range(len(nodes)):
-        if (nodes[index].path_cost + nodes[index].heuristic_cost) < lowest_cost:
-            lowest_cost = (nodes[index].path_cost + nodes[index].heuristic_cost)
-    return nodes[index_node_to_pop], index_node_to_pop
+def contain(node, nodes):
+    for i in range(len(nodes)):
+        if node.problem.puzzle == nodes[i].problem.puzzle:
+            return True
+    return False
+# ------------------------------------------------------------------------------ #
 
+# function GRAPH-SEARCH(problem) returns a solution, or failure
+#     initialize the frontier using the initial state of problem
+#     initialize the explored set to be empty
+#     loop do
+#         if the frontier is empty then return failure
+#         choose a leaf node and remove it from the frontier
+#         if the node contains a goal state then return the corresponding solution
+#         add the node to the explored set
+#         expand the chosen node, adding the resulting nodes to the frontier
+#             only if not in the frontier or explored set
+
+# ---------------------- Uniform Cost Search ----------------------  #
 def ucs(problem):
-    node = Node(problem.puzzle, 0, 0)
+    global max_q_size
+    node = Node(problem, 0, 0)
+    # initialize the frontier using the initial state of problem
     frontier = [node]
+    # initialize the explored set to be empty
+    explored_set = []
 
-    # frontier is empty -> failed
-    if not bool(frontier):
-        return []
+    # loop do
+    while True:
+        max_q_size = max(len(frontier), max_q_size)
+        # if the frontier is empty then return failure
+        if not bool(frontier):
+            return [], expanded_children, max_q_size
+
+        # choose a leaf node and remove it from the frontier
+        node = Node.deque(frontier)
+        # if the node contains a goal state then return the corresponding solution
+        if node.problem.puzzle == node.problem.goal_state:
+            return node, expanded_children, max_q_size
+
+        print('The best state to expand with a g(n) = ', node.path_cost, 'and h(n) = 0 is...')
+        print_state(node.problem.puzzle)
+        print('\tExpanding this node...\n')
+        # add the node to the explored set
+        explored_set.append(node)
+        # expand the chosen node, adding the resulting nodes (new children) to the frontier
+        new_children_nodes = Node.expand(node)
+        for child in new_children_nodes:
+            in_expand_list = contain(child, frontier)
+            in_explored_list = contain(child, explored_set)
+            # only if not in the frontier or explored set
+            if not in_expand_list and not in_explored_list:
+                frontier.append(child)
+# ------------------------------------------------------------------------------ #
+    
+
+# ---------------------- A* with Misplaced Tile Heuristic ----------------------  #
+def find_misplaced_heuristic(node):
+    num_misplaced = 0
+    for row in range(len(node.problem.puzzle)):
+        for col in range(len(node.problem.puzzle[row])):
+            if node.problem.puzzle[row][col] != node.problem.goal_state[row][col]:
+                # The blank can't really be a "misplaced" tile, only the elem that the blank space took over
+                if node.problem.puzzle[row][col] != 0:
+                    num_misplaced += 1
+    return num_misplaced
 
 def misplaced(problem):
-    print('called misplaced')
+    global max_q_size
+    node = Node(problem, 0, 0)
+    frontier = [node]
+    explored_set = []
 
-def heuristic(problem):
-    print('called heuristic')
+    node.heuristic_cost = find_misplaced_heuristic(node)
+
+    while True:
+        max_q_size = max(len(frontier), max_q_size)
+        # frontier is empty -> fail
+        if not bool(frontier):
+            return [], 0, 0
+
+        # Choose the next node
+        node = Node.deque(frontier)
+        if node.problem.puzzle == problem.goal_state:
+            return node, expanded_children, max_q_size
+        print('The best state to expand with a g(n) =', node.path_cost, 'and h(n) =', node.heuristic_cost, 'is...')
+        print_state(node.problem.puzzle)
+        print('\tExpanding this node...\n')
+        explored_set.append(node)
+        new_children_nodes = Node.expand(node)
+        for child in new_children_nodes:
+            child.heuristic_cost = find_misplaced_heuristic(child)
+            in_expand_list = contain(child, frontier)
+            in_explored_list = contain(child, explored_set)
+            if not in_expand_list and not in_explored_list:
+                frontier.append(child)
+# ------------------------------------------------------------------------------ #
+
+
+# ---------------------- A* with Euclidean Distance Heuristic ----------------------  #        
+def find_correct_position(puzzle, val):
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            if puzzle[i][j] == val:
+                return i,j
+
+def calc_euclidean_heuristic(node):
+    curr_node = node.problem.puzzle
+    goal_state = node.problem.goal_state
+    eucl_heuristic_sum = 0
+    for x1 in range(len(curr_node)):
+        for y1 in range(len(curr_node[x1])):
+                if curr_node[x1][y1] != 0 and curr_node[x1][y1] != goal_state[x1][y1]:
+                    (x2, y2) = find_correct_position(goal_state, curr_node[x1][y1])
+                    eucl_heuristic_sum += math.ceil(math.sqrt(pow(x2 - x1,2) + pow(y2 - y1, 2)))
+    return eucl_heuristic_sum
+
+def euclidean(problem):
+    global max_q_size
+    node = Node(problem, 0, 0)
+    frontier = [node]
+    explored_set = []
+
+    node.heuristic_cost = calc_euclidean_heuristic(node)
+
+    while True:
+        max_q_size = max(len(frontier), max_q_size)
+        # frontier is empty -> fail
+        if not bool(frontier):
+            return [], 0, 0
+
+        # Choose the next node
+        node = Node.deque(frontier)
+        if node.problem.puzzle == problem.goal_state:
+            return node, expanded_children, max_q_size
+        print('The best state to expand with a g(n) =', node.path_cost, 'and h(n) =', node.heuristic_cost, 'is...')
+        print_state(node.problem.puzzle)
+        print('\tExpanding this node...\n')
+        explored_set.append(node)
+        new_children_nodes = Node.expand(node)
+        for child in new_children_nodes:
+            child.heuristic_cost = calc_euclidean_heuristic(child)
+            in_expand_list = contain(child, frontier)
+            in_explored_list = contain(child, explored_set)
+            if not in_expand_list and not in_explored_list:
+                frontier.append(child)
